406b76beeb14afcd98f55be2d9da68814a328bc5
Johannes Rieke <johannes.rieke@gmail.com>
2021-01-03 22:06:47 +0100

    Initial prototype

---
+++ example.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit_analytics
+
+with streamlit_analytics.track():
+    st.title("Just a small test app")
+    text = st.text_input("Enter some text")
+    selected = st.selectbox("Select an option", ("option 1", "option 2", "option 3"))
+    clicked = st.button("Click here")
---
+++ streamlit_analytics/__init__.py
@@ -0,0 +1,1 @@
+from .main import track, start_tracking, stop_tracking, counts
