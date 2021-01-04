69256ecd781d7c8231ea63ddb7f12b9fdb7534c9
Johannes Rieke <johannes.rieke@gmail.com>
2021-01-04 22:13:32 +0100

    Style plot and labels

--- streamlit_analytics/main.py
+++ streamlit_analytics/main.py
@@ -354,7 +354,13 @@ def stop_tracking(
 
             st.header("Traffic")
             st.write(
-                f"**Total:** {counts['total_pageviews']} pageviews, {counts['total_script_runs']} script runs"
+                f"""
+                Total: {counts['total_pageviews']} pageviews, {counts['total_script_runs']} script runs
+                <br>
+                <sub>pageview = every time a user goes on the site or reloads; 
+                script run = every time a widget changes and streamlit re-runs the script</sub>
+                """,
+                unsafe_allow_html=True
             )
 
             # Plot chart with pageviews.
@@ -365,25 +371,44 @@ def stop_tracking(
             line1 = base.mark_line(point=True, stroke="#5276A7").encode(
                 alt.Y(
                     "pageviews:Q",
-                    axis=alt.Axis(titleColor="#5276A7", format=".0f", tickMinStep=1),
+                    axis=alt.Axis(
+                        titleColor="#5276A7",
+                        tickColor="#5276A7",
+                        labelColor="#5276A7",
+                        format=".0f",
+                        tickMinStep=1,
+                    ),
                     scale=alt.Scale(domain=(0, df["pageviews"].max() + 1)),
                 )
             )
             line2 = base.mark_line(point=True, stroke="#57A44C").encode(
                 alt.Y(
                     "script_runs:Q",
-                    axis=alt.Axis(titleColor="#57A44C", format=".0f", tickMinStep=1),
+                    axis=alt.Axis(
+                        title="script runs",
+                        titleColor="#57A44C",
+                        tickColor="#57A44C",
+                        labelColor="#57A44C",
+                        format=".0f",
+                        tickMinStep=1,
+                    ),
                 )
             )
-            layer = alt.layer(line1, line2).resolve_scale(y="independent")
+            layer = (
+                alt.layer(line1, line2)
+                .resolve_scale(y="independent")
+                .configure_axis(titleFontSize=15, labelFontSize=12, titlePadding=10)
+            )
             st.altair_chart(layer, use_container_width=True)
 
             st.header("Widget interactions")
             st.markdown(
                 """
                 Numbers represent user interactions, e.g. how often a button was 
-                clicked. Note that they only increase when the state of the widget
-                changes, not every time streamlit runs the script.
+                clicked, how often a specific text input was given, ...
+                <br>
+                <sub>Note: Numbers only increase if the state of the widget
+                changes, not every time streamlit runs the script.</sub>
                 """,
                 unsafe_allow_html=True,
             )
