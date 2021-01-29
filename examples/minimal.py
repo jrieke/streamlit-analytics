b7fc12fa0f6a563faa7bfa74e43a15bb55ff5d28
Johannes Rieke <johannes.rieke@gmail.com>
2021-01-30 00:20:30 +0100

    Make minimal example minimal again

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import streamlit_analytics
 
-
-with streamlit_analytics.track():  # firestore_key_file="firestore-key.json"):
-
-    streamlit_analytics.counts["widgets"] = {"Write your name": {"foo": 1}}
+with streamlit_analytics.track():
     st.text_input("Write your name")
     st.selectbox("Select your favorite", ["cat", "dog", "flower"])
     st.button("Click me")
